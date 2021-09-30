1e0798d5b77fe79ff73f70eec9f3afe3f142ee67
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-09-30 09:33:02 -0700

    Fix: 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	printf(stderr, "Size of a char: %lu byte(s)\n", sizeof(char));
-	printf(stderr, "Size of a int: %lu byte(s)\n", sizeof(int));
-	printf(stderr, "Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf(stderr, "Size of a long long int: %lu byte(s)\n",
+	printf(stdout, "Size of a char: %lu byte(s)\n", sizeof(char));
+	printf(stdout, "Size of a int: %lu byte(s)\n", sizeof(int));
+	printf(stdout, "Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf(stdout, "Size of a long long int: %lu byte(s)\n",
 			sizeof(long long int));
-	printf(stderr, "Size of a float: %lu byte(s)\n", sizeof(float));
+	printf(stdout, "Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
