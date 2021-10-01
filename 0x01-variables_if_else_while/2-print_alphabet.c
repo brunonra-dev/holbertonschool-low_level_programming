fe3c6bfbd3a7e8973289aa5aacb865e7b27645a7
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-01 10:04:14 -0700

    Fix: 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Print if a number is positive or negative
+ *Return: 0
+ */
+int main(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z')
+	{
+		putchar(abc);
+		abc++;
+	}
+	putchar('\n');
+	return (0);
+}
