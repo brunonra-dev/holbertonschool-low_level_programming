ed4077ff7daf268f2e1f76f414b6cf7abb168317
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-05 10:13:42 -0700

    Added: 4-isalpha.c file && Update: main.h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *_isalpha - function that checks for alphabetic character
+ *@c: The character to compere
+ *Return: 1 if c is alphabetic character
+ *On error: 0 is returned
+ */
+int _isalpha(int c)
+{
+	char abc = 'a';
+	char abc_may = 'A';
+	int ret = 0;
+
+	while (abc <= 'z')
+	{
+		if (c == abc)
+		{
+			ret = 1;
+		}
+		abc++;
+	}
+
+	while (abc_may <= 'Z')
+	{
+		if (c == abc_may)
+		{
+			ret = 1;
+		}
+		abc_may++;
+	}
+	return (ret);
+}
