526972e30b7f75c73f79208b8f9e29e8ec74028a
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-05 10:40:25 -0700

    Added: 5-sign.c file & Update: main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - function prints the sign of a number
+ *@n: The character to compere
+ *Return: 1 if c is alphabetic character
+ *On error: 0 is returned
+ */
+int print_sign(int n)
+{
+	int ret = 0;
+
+	if (n > 0)
+	{
+		ret = 1;
+		_putchar('+');
+	} else if (n == 0)
+	{
+		ret = 0;
+		_putchar('0');
+	} else
+	{
+		ret = -1;
+		_putchar('-');
+	}
+	return (ret);
+}
