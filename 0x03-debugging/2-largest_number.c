71a0444990a682482da17b8bccd47bfe5ef772e1
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-06 08:56:35 -0700

    Fix: 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && a > b)
+	else if (c > a)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = a;
 	}
 
 	return (largest);
