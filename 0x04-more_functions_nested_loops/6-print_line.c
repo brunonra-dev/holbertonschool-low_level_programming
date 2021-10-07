6f9b647f5560418ab9a914e6ca8e52759efd84b1
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-07 16:19:57 -0700

    Added: 6-print_line.c & update: main.h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_line - function that draws a straight line in the termina.
+ *@n: n times '_'
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
