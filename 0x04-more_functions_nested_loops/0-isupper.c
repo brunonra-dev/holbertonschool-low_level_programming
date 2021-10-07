1b6b94b58c8c9735c8bd91bf134641e0936d4317
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-07 10:06:03 -0700

    Fix: 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *_islower - function that checks for uppercase character
+ *_isupper - function that checks for uppercase character
  *@c: The character to compere
  *Return: 1 if c is uppercase
  *On error: 0 is returned
  */
-int _islower(int c)
+int _isupper(int c)
 {
 	char abc = 'A';
 	int ret = 0;
