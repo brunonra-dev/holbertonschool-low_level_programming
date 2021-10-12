f27d9cdabe5e9bc1fb939146c4949e9906e31a11
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-12 10:29:46 -0700

    Added: 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - length of string
+ *
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
