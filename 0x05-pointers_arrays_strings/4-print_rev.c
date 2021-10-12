f4d0aa198b97d2a89962cec9b01f3c7c13aaa1db
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-12 11:03:08 -0700

    Added: 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ *
+ * @str: string
+ */
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	while (len < 0)
+	{
+		_putchar(str[len]);
+		len--;
+	}
+	_putchar('\n');
+}
