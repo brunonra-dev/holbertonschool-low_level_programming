15209850357709dbb248b5153a2487fe53e9239a
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-12 17:19:21 -0700

    Fix: 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,17 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int half, i;
+	int half;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	half = (len - 1) / 2;
-	for (i = half; i > len; i++)
+	half = len / 2;
+	while (half < len)
 	{
-		_putchar(str[i]);
+		_putchar(str[half]);
+		half++;
 	}
 	_putchar('\n');
 }
