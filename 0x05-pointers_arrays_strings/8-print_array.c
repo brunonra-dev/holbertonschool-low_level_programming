16229b0f34e6a2d77449687df32c3cb2814f3b87
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-12 19:53:14 -0700

    Added: 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_array - print array
+ *
+ * @a: string
+ * @n: nelements of an array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i >= n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
