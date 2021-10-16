4d84411c8622588cc2b869b1cb9c28da3f6c495b
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-15 17:22:23 -0700

    Fix: now work like strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,22 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len, s2_len, i, sum1, sum2;
+	int s1_len, s2_len, i, ret;
 
 	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		sum1 += s1[s1_len];
 
 	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		sum2 += s2[s2_len];
 
-	i = sum1 - sum2;
+	for (i = 0; i < s1_len; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			ret = s1[i] - s2[i];
+			break;
+		}
+		else
+			ret = 0;
+	}
 
-	return (i);
+	return (ret);
 }
