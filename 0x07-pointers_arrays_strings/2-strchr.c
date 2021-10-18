e9b4329d1c007ac49ac49df2676c57f80b59d296
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-18 10:31:30 -0700

    Fix: 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,13 @@ char *_strchr(char *s, char c)
 	int i;
 	char *p = '\0';
 
-	if (c != '\0')
-	{
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
+		{
 			p = &s[i];
-	}
+			break;
+		}
 	}
 	return (p);
 }
