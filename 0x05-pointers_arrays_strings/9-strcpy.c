304d8d20b3c549a3551f90c8ceb803ed1e4397cd
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-22 15:47:56 -0700

    Fix: 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * *_strcpy - copy array
- *
- * @dest: string dest
- * @src: string src.
- *
- * Return: 0 on success
- */
+  * *_strcpy - copy string.
+  *
+  * @src: string src
+  * @dest: string dest
+  *
+  * Return: the pointer to dest
+  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
+	i = 0;
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-
 	dest[i] = '\0';
 
-	return (0);
+	return (dest);
 }
