798de5f94a2193303d1314893afa20659fd3b427
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-26 10:43:56 -0700

    Fix: 2-str_concat.c
    NULL strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,6 @@
  *
  * Return: pointer to the concatenated array, or NULL if it fails.
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *con_str;
@@ -20,34 +19,33 @@ char *str_concat(char *s1, char *s2)
 	{
 		for (s1_len = 0; s1[s1_len] != 0; s1_len++)
 			;
-
 		for (s2_len = 0; s2[s2_len] != 0; s2_len++)
 			;
-
-		con_str = malloc(s1_len + s2_len + 1);
-
-		if (con_str == 0)
-		{
-			return (NULL);
-		}
-		else
-		{
-			while (i <= s1_len)
-			{
-				con_str[i] = s1[i];
-				i++;
-			}
-
-			while (j <= s2_len)
-			{
-				con_str[j + s1_len] = s2[j];
-				j++;
-			}
-
-			return (con_str);
-		}
 	}
 	else
+	{
+		s1_len = 1;
+		s2_len = 2;
+	}
+	con_str = malloc(s1_len + s2_len + 1);
+	if (con_str == 0)
+	{
 		return (NULL);
+	}
+	else
+	{
+		while (i <= s1_len)
+		{
+			con_str[i] = s1[i];
+			i++;
+		}
 
+		while (j <= s2_len)
+		{
+			con_str[j + s1_len] = s2[j];
+			j++;
+		}
+
+		return (con_str);
+	}
 }
