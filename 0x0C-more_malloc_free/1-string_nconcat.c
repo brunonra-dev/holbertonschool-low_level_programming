76c1303ed7fa7adfc012222c910f985c12b551f9
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-10-28 10:28:35 -0700

    Fix: loop

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,7 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i <= s1_len; i++)
 		con_str[i] = s1[i];
 
-	for (j = 0; j < s2_len; j++)
+	for (j = 0; j <= s2_len; j++)
 		con_str[j + s1_len] = s2[j];
 
 	con_str[j + s1_len] = '\0';
