3b4f87a88223862e7f882281fb4c8a3c8c4a0ef5
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-01 19:41:00 -0700

    Added: free dog function & update: header

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog structure.
+ *
+ * @d: pointer;
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
