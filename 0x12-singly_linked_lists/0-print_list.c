83e55ddeef4a1781fe1746447ebcb1c69f8143d6
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-11 09:41:44 -0800

    Fix: while statement

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,19 +12,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t numb_node = 0;
+	int numb_node = 0;
 
 	if (!h)
 		return (0);
 
-	while (!h->next)
+	while (h != NULL)
 	{
 		if (!h->str)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		numb_node += 1;
+		h = h->next;
 	}
 
 	return (numb_node);
