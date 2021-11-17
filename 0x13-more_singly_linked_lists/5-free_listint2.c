09b4b3c66585ba490982ed54b6021ac0bc0fbf7f
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-16 16:08:43 -0800

    Added: free_listint2 function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free linked list
+ *
+ * @head: pointer to list_t
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = *head;
+		(*head) = (*head)->next;
+		free(tmp);
+	}
+
+	head = NULL;
+}
