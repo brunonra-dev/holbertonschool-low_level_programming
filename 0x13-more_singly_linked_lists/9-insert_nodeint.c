30408270d10ac40ed72337fb9b1ffc7744cd79f3
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-16 18:14:33 -0800

    Revert "fix"
    This reverts commit d8c935b738b74c289b4937bd3e93298e1e36a498.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,11 +45,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		idx_node++;
 	}
 
-	if (idx_node > idx)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
 	return (new_node);
 }
