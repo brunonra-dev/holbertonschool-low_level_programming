6fa86844c4e0c4cfed131b2f2476ce32ead95a03
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-16 16:38:03 -0800

    Added: get_nodeint function

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ *
+ * @head: pointer to list_t
+ * @index:  is the index of the node
+ *
+ * Return: nth node of a listint_t linked list
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	tmp = head;
+
+	for (i = 0; i < index; i++)
+	{
+		tmp = tmp->next;
+	}
+
+	return (tmp);
+}
