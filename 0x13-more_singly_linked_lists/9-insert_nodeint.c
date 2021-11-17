e47e300d2bafc3c707e382a5fc755e5b79bf57d7
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-16 18:00:42 -0800

    Fix: return

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!tmp)
 	{
 		tmp = new_node;
-		return (tmp);
+		return (new_node);
 	}
 
 	while (idx_node <= idx && tmp->next)
