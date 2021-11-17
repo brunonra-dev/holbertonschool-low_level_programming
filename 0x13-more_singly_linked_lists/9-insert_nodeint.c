d284763f15ff5d9c3fcdfa695acf479b3ff48f0d
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-16 17:18:24 -0800

    Fix: statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,11 +15,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp, *new_node, *past_node;
 	unsigned int idx_node = 0;
 
-	if (!head)
-		return (NULL);
-
-	tmp = *head;
-
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 	{
@@ -29,7 +24,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	while (!(idx == idx_node))
+	tmp = *head;
+
+	if (!tmp)
+	{
+		tmp = new_node;
+		return (tmp);
+	}
+
+	while (idx_node <= idx)
 	{
 		if ((idx - 1) == idx_node)
 			past_node = tmp;
