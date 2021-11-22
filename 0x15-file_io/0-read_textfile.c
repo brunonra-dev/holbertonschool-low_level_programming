9a970505792003c21c0c6514f64a7781e0518031
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-22 11:17:49 -0800

    Added read_textfile function

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: pointer to file
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int rp = 0, file;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+
+	buffer = malloc(letters);
+	if (!buffer)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	rp = write(STDOUT_FILENO, buffer, rp);
+	if (rp == -1)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
+
+	free(buffer);
+	close(file);
+	return (rp);
+}
