98c944306b9c36fc793c3709bdae9ab8f3dd5a3c
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-22 17:19:55 -0800

    Fix return on text_content NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		return (-1);
+		return (1);
 
 	file = open(filename, O_APPEND | O_WRONLY);
 	if (file == -1)
