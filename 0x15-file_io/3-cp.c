0acac1cb4608d765e90ba9b38256a680d5c9c6a8
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-23 07:42:25 -0800

    Fix close files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,10 +36,18 @@ int main(int ac, char **av)
 		exit(99);
 	}
 	rp = read(file_from, buffer, 1024);
-
+	if (rp == -1)
+	{
+		close_file(file_from);
+		close_file(file_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	rp = write(file_to, buffer, rp);
 	if (rp == -1)
 	{
+		close_file(file_from);
+		close_file(file_to);
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
