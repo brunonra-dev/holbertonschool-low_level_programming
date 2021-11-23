a5c4ec6604a473f5f567fce6c87ae3b8a7619801
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-22 17:13:30 -0800

    Added append_text_to_file function

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ *
+ * @filename: pointer to file
+ * @text_content: text to file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int wf, file;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		return (-1);
+
+	file = open(filename, O_APPEND | O_WRONLY);
+	if (file == -1)
+		return (-1);
+
+	wf = write(file, text_content, strlen(text_content));
+	if (wf == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
