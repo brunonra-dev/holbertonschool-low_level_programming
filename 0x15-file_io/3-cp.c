7b7bac6a695479ed4284d2d546373ed7d31d4704
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-23 08:25:25 -0800

    Fix stderr

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,7 +32,7 @@ int main(int ac, char **av)
 	if (file_to == -1)
 	{
 		close_file(file_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	while (rp)
@@ -42,7 +42,7 @@ int main(int ac, char **av)
 		{
 			close_file(file_from);
 			close_file(file_to);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		rp = write(file_to, buffer, rp);
