5541d6a02528f953207069f073d5daf33fc5c364
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2021-11-24 08:41:44 -0800

    Added elf function

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,16 @@
+/**
+ * main - displays the information contained in the ELF header at
+ * the start of an ELF file
+ *
+ * @ac: argument count
+ * @av: arguments
+ *
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+
+
+	return (0);
+}
