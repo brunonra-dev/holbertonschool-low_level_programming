4285df6e1e5baa192dfdbfa82a4eebebc2243cb9
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-27 08:15:53 -0800

    added 0-hash_table_create.c & 1-djb2.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable = NULL;
+
+	/*allocate table*/
+	hashtable = malloc(sizeof(hash_table_t));
+	if (!hashtable)
+		return (NULL);
+
+	/*allocate table entries*/
+	hashtable->array = malloc(sizeof(hash_node_t) * size);
+	if (!hashtable->array)
+	{
+		free(hashtable);
+		return (NULL);
+	}
+
+	return (hashtable);
+}
