9986f0df7e9479a0228b6c69e1724d735adfe224
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-28 06:54:41 -0800

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/*allocate table entries*/
 	hashtable->size = size;
-	hashtable->array = malloc(sizeof(hash_node_t) * size);
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (!hashtable->array)
 	{
 		free(hashtable);
