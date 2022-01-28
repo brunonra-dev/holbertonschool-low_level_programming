5453484518bd77f5cf525d17dfe129b3db08a6b3
Bruno Rodriguez <40237383+brunonra-dev@users.noreply.github.com>
2022-01-28 11:42:57 -0800

    added 6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ *
+ * @ht: hashtable
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *item, *tmp;
+	unsigned long int i = 0;
+
+	if (!ht)
+		return;
+
+	for (; i < (ht->size); ++i)
+	{
+		item = ht->array[i];
+		while (item)
+		{
+			tmp = item;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			item = item->next;
+		}
+	} 
+	free(ht->array);
+	free(ht);
+}
