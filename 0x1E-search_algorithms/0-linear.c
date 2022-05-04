e4d556d1309fd7c69482954314e36fb7f10a5946
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-04 15:47:19 -0300

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * using the Linear search algorithm
+ *
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search
+ *
+ * Return: index
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0, length = size;
+
+	if (!array)
+		return (-1);
+
+	for (; i < length; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
